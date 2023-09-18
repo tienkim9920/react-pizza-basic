2fdeadc6d7fe0bb4aa58328f9c09f833c166c5ae
Tien Kim <tienkim9920@gmail.com>
2023-09-18 22:57:23 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,31 @@
+import { useState } from "react";
+
 const App = () => {
+
+  const [name, setName] = useState('Nine Dev');
+
+  const nameList = ['Nine Dev', '2023', 'wwww'];
+  const nameObj = {
+    name: 'Nine Dev',
+    old: 2023,
+    email: 'tienkim9920@gmail.com',
+  }
+
+  const handleChangeName = () => {
+    setName('Nine Dev Welcome');
+  }
+
+  console.log("Render");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{name}</div>
+      <div>{nameList}</div>
+      <div>{nameObj.email}</div>
+      <div>{nameObj.old}</div>
+      <div>{nameObj.name}</div>
+
+      <button onClick={handleChangeName}>Change Name</button>
     </div>
   );
 }
